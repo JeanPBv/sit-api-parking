4887bd004923dcdcbd291510ff85fd4d93bb71ec
JeanPBv <jbocanegrav1@upao.edu.pe>
2023-04-17 13:25:18 -0500

    feat:Clase completa de ParkingController, incluyendo nuevos metodos

--- api-parking/api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
@@ -1,19 +1,22 @@
 package com.parking.apiparking.controller;
 
 import com.parking.apiparking.entity.Car;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.parking.apiparking.services.ParkingService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController //Notación que indica el comportamiento al acceso a los verbos hhtp a los status de respuesta y cada metodo tendra una URL que identificará al recurso que quiere acceder
 @RequestMapping("/parking") //parking -- URL
 
 public class ParkingController {
 
-    private final ParkingService parkingService;
+    @Autowired
+    private ParkingService parkingService;
 
     public ParkingController(ParkingService parkingService){
         this.parkingService = parkingService;
@@ -24,6 +27,23 @@ public class ParkingController {
     public ResponseEntity<List<Car>> getAllCars(){
         return ResponseEntity.ok(this.parkingService.getAllCars());
     }
+    @GetMapping("/cars/{licensePlate}")
+    public ResponseEntity<Car> getCarByLicensePlate(@PathVariable String licensePlate){
+       Optional<Car> optionalCar = this.parkingService.getCarByLicensePlate(licensePlate);
+       if(optionalCar.isPresent()){
+           return ResponseEntity.ok(optionalCar.get());
+       }
+       return ResponseEntity.notFound().build();
+   }
+
+   @GetMapping("/cars")
+   public ResponseEntity<List<Car>> getCarsByColor(@RequestParam(required = false) String color){
+        if(color !=null){
+            List<Car> carsByColor = this.parkingService.gerCarsByColor(color);
+            return ResponseEntity.ok(carsByColor);
+        }
+        return ResponseEntity.ok(this.parkingService.getAllCars());
+   }
 
     //POST: http://dominio/parkings/cars
     @PostMapping("/cars")
@@ -32,5 +52,26 @@ public class ParkingController {
        return new ResponseEntity<>(car, HttpStatus.CREATED);
     }
 
+    @DeleteMapping("/cars/{licensePlate}")
+    public ResponseEntity<Void> removeCarByLicensePlate(@PathVariable String licensePlate){
+        boolean removed = this.parkingService.removeCarByLicensePlate(licensePlate);
+        if(removed){
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/cars/{licensePlate}/entry")
+    public ResponseEntity<Void> registerEntry(@PathVariable String licensePlate){
+        Car car = new Car(licensePlate,null);
+        this.parkingService.parkCar(car);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    public ResponseEntity<Double> registerExit(@PathVariable String licensePlate){
+        double fee = this.parkingService.calculateParkingFee(licensePlate);
+        this.parkingService.unparkCar(licensePlate);
+        return ResponseEntity.ok(fee);
+    }
 
 }
